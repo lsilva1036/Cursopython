c4b151688fa6f6669c944e768532392eca6141b3
lsilva1036 <39881913+lsilva1036@users.noreply.github.com>
2019-11-26 17:08:40 -0500

    Add files via upload

---
+++ Ejemplo1.py
@@ -0,0 +1,6 @@
+Python 3.7.5rc1 (tags/v3.7.5rc1:4082f600a5, Oct  1 2019, 20:28:14) [MSC v.1916 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> f = 5 + 4
+>>> print(f)
+9
+>>> 
